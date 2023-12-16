f66b926036a7a8ae9fd08fe82c12694706b12282
Jacob Hirschi <hirschij@oregonstate.edu>
2023-12-15 16:35:37 -0800

    fixed linebreaks in NB1 docstring

--- notebooks/notebook_functions/NB1_functions.py
+++ notebooks/notebook_functions/NB1_functions.py
@@ -24,10 +24,12 @@ def psi_reg(x, y, z, q_nx=1, q_ny=1, q_nz=1, lx=1, ly=1, lz=1):
     >>> psi_reg(1.5, 1.5, 1.5, 1, 1, 1, 3, 3, 3)
     0.5443310539518174
     '''
-    wvfn = np.sqrt(8/(lx*ly*lz)) * \
-        np.sin((q_nx*np.pi*x)/lx) * \
-        np.sin((q_ny*np.pi*y)/ly) * \
-        np.sin((q_nz*np.pi*z)/lz)
+    wvfn = (
+        np.sqrt(8 / (lx * ly * lz)) *
+        np.sin((q_nx * np.pi * x) / lx) *
+        np.sin((q_ny * np.pi * y) / ly) *
+        np.sin((q_nz * np.pi * z) / lz)
+    )
     return wvfn
 
 def psi_ener(qnx, qny, qnz, lx, ly, lz):
