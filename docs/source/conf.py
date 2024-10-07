699f4e9952531b8bce5b14ce44e94f575b7589d5
Jacob Hirschi <hirschij@oregonstate.edu>
2024-10-07 10:03:38 -0700

    updated doc config files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,7 @@ extensions = [ 'sphinx.ext.autodoc',
                 'sphinx.ext.intersphinx',
                 'sphinx_design',
                 'sphinxcontrib.images',
+                "sphinx_build_compatibility.extension",
                ]
 
 templates_path = ['_templates']
